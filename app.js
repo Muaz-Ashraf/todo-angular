9104b0eca1d85e3a48699eee4d464aaf0f61c8ba
Muaz Ashraf <muazashrafnet@gmail.com>
2023-08-25 14:50:28 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+// app.js
+var app = angular.module("todoApp", []);
+
+app.controller("TodoController", function ($scope) {
+  $scope.tasks = [];
+
+  $scope.addTask = function () {
+    if ($scope.newTask) {
+      $scope.tasks.push({ text: $scope.newTask, done: false });
+      $scope.newTask = "";
+    }
+  };
+  $scope.handleKeyPress = function (event) {
+    if (event.keyCode === 13) {
+      $scope.addTask();
+    }
+  };
+
+  $scope.removeTask = function (task) {
+    var index = $scope.tasks.indexOf(task);
+    if (index !== -1) {
+      // Set the 'removing' property to true for the task
+      $scope.tasks[index].removing = true;
+      setTimeout(function () {
+        $scope.$apply(function () {
+          $scope.tasks.splice(index, 1);
+        });
+      }, 200);
+    }
+  };
+  $scope.removeCompletedTasks = function () {
+    var tasksToRemove = [];
+    for (var i = 0; i < $scope.tasks.length; i++) {
+      if ($scope.tasks[i].done) {
+        tasksToRemove.push($scope.tasks[i]);
+      }
+    }
+    for (var i = 0; i < tasksToRemove.length; i++) {
+      var index = $scope.tasks.indexOf(tasksToRemove[i]);
+      if (index !== -1) {
+        $scope.tasks.splice(index, 1);
+      }
+    }
+  };
+  $scope.hasCompletedTasks = function () {
+    for (var i = 0; i < $scope.tasks.length; i++) {
+      if ($scope.tasks[i].done) {
+        return true; // Return true as soon as a completed task is found
+      }
+    }
+    return false; // Return false if no completed tasks are found
+  };
+});
